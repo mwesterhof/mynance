f798a3368f965b69dc0ab3cb6b9eddca6d7e2c97
Marco Westerhof <m.westerhof@lukkien.com>
2022-10-18 17:15:02 +0200

    things

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from screens import MynanceScreenManager
+
+
+Window.keyboard_anim_args = {"d":.2,"t":"linear"}
+Window.softinput_mode = "below_target"
 
 
 class TitleBar(BoxLayout):
